020f34047ee0fac172ad94c97b0bbb9980660424
Josh Reid <josh@reiddesign.co.nz>
2018-04-05 16:56:23 +1200

    Menu toggle and active state with .active class - commented out ajax on .systemmenu click as was causing errors - may not need this if you use the .active class

--- www/js/menu.js
+++ www/js/menu.js
@@ -1,17 +1,15 @@
 jQuery(document).ready(function() {
 	
-	$(".systemmenu").click(function() {
+	/* $(".systemmenu").click(function() {
 		$(".systemsubmenu").toggle();
 		var state=$(".systemsubmenu").css("display");
 		$.post( "/ajax/functions.php", {'state': state, 'updateMenuState': 1}, function( data ) {
-		  
-		 
 		  console.log(data);
-		  
-		  
-		  
-		  
 		});
+	}); */
+
+	$(document).on('click','.menu-button-wrap .toggle-menu',function(){
+		$('nav').toggleClass('show-mobile-menu');
 	});
 	
 });
